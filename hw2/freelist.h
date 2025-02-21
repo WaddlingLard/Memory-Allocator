f0d20351a5bc630b7e078e1fa16fbf547d1854b2
Brian <dathamor@gmail.com>
2025-02-21 06:03:24 -0700

    Header adjustment

--- hw2/freelist.h
+++ hw2/freelist.h
@@ -5,11 +5,11 @@
 
 typedef void *FreeList;
 
-extern FreeList freelistcreate(size_t size, int l, int u);
-extern void     freelistdelete(FreeList f, int l, int u);
+extern FreeList freelistcreate(size_t size, int l, int u, void *base);
+extern void freelistdelete(FreeList f, int l, int u);
 
 extern void *freelistalloc(FreeList f, void *base, int e, int l);
-extern void  freelistfree(FreeList f, void *base, void *mem, int e, int l);
+extern void freelistfree(FreeList f, void *base, void *mem, int e, int l);
 
 extern int freelistsize(FreeList f, void *base, void *mem, int l, int u);
 extern void freelistprint(FreeList f, int l, int u);
